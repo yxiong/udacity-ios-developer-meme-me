7be823e7eded3c2dd6469e9ae957039cc014ea82
Ying Xiong <ying@xiongs.org>
2015-09-27 21:53:48 -0700

    Add image picker delegate to show the image in the view

--- Meme Me/ViewController.swift
+++ Meme Me/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     
@@ -16,9 +16,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            self.imagePickerView.image = image
+        } else {
+            print("No image found")
+        }
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     @IBAction func pickAnImage(sender: AnyObject) {
         let pickerController = UIImagePickerController()
+        pickerController.delegate = self
         self.presentViewController(pickerController, animated: true, completion: nil)
     }
 
