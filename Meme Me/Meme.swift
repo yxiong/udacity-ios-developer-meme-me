21b17bce011afdfe30edb3da7c1c77d52b0001c7
Ying Xiong <ying@xiongs.org>
2015-09-29 17:15:16 -0700

    Change Meme from a class to a struct

--- Meme Me/Meme.swift
+++ Meme Me/Meme.swift
@@ -9,16 +9,9 @@
 import Foundation
 import UIKit
 
-class Meme {
+struct Meme {
     let topText: String
     let bottomText: String
     let image: UIImage
     let memedImage: UIImage
-    
-    init(topText: String, bottomText: String, image: UIImage, memedImage: UIImage) {
-        self.topText = topText
-        self.bottomText = bottomText
-        self.image = image
-        self.memedImage = memedImage
-    }
 }
